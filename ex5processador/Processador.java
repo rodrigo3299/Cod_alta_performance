d45f7962b1f6c1be420e3f89c386fc501a7cb038
rodrigo3299 <99183265+rodrigo3299@users.noreply.github.com>
2026-04-15 20:31:28 -0300

    Add files via upload

---
+++ ex5processador/Processador.java
@@ -0,0 +1,43 @@
+package ex5processador;
+
+import ex4.FilaInt;
+
+import java.util.Scanner;
+
+public class Processador {
+    public static void main(String[] args) {
+        Scanner le = new Scanner(System.in);
+        FilaInt fila = new FilaInt();
+        fila.init();
+        int opcao;
+        do{
+            System.out.println("1 - Submeter processo para excução");
+            System.out.println("2 - Executar processo");
+            System.out.println("3 - Shutdown");
+            opcao = le.nextInt();
+            switch (opcao){
+                case 1:
+                    System.out.println("Informe pid: ");
+                    int pid = le.nextInt();
+                    fila.enqueue(pid);
+                    break;
+                case 2:
+                    if(fila.isEmpty()){
+                        System.out.println("Não há processos aguardando na fila");
+                    } else{
+                        pid = fila.dequeue();
+                        System.out.println("Process: " +pid+ " será executado agora");
+                    }
+                    break;
+                case 3:
+                    if(!fila.isEmpty()){
+                        System.out.println("Há alunos aguardando na fila");
+                        opcao = 0;
+                    }
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        } while (opcao!=3);
+    }
+}
